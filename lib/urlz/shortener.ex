7a25866cfdb5b5b001c328e44b64bc781ad19549
Jearvon Dharrie <j.dharrie@gmail.com>
2017-06-23 15:37:52 -0400

    Add URL expansion
    Save shortened urls in an Agent

--- lib/urlz/shortener.ex
+++ lib/urlz/shortener.ex
@@ -1,9 +1,24 @@
 defmodule Urlz.Shortener do
-  @spec shorten(String.t) :: String.t
-  def shorten(url) do
-    url
-    |> :erlang.md5
-    |> Base.encode16(case: :lower)
-    |> String.slice(0..3)
+
+  @spec start_link() :: {:ok, pid} 
+  def start_link do
+    Agent.start_link(fn -> %{} end)
+  end
+  
+  @spec shorten(any, String.t) :: String.t
+  def shorten(db, url) do
+    shortened_url = 
+      url
+      |> :erlang.md5
+      |> Base.encode16(case: :lower)
+      |> String.slice(0..3)
+
+    Agent.update(db, fn(urls) -> Map.put(urls, shortened_url, url) end)
+    shortened_url
+  end
+
+  @spec expand(any, String.t) :: String.t
+  def expand(db, shortened_url) do
+    Agent.get(db, fn(urls) -> Map.get(urls, shortened_url) end)
   end
 end
--- test/urlz/urlz_shortener_test.exs
+++ test/urlz/urlz_shortener_test.exs
@@ -2,11 +2,21 @@ defmodule Urlz.ShortenerTest do
   use ExUnit.Case, async: true
 
   test "shortens a url" do
+    {:ok, db}= Urlz.Shortener.start_link
     url =  "http://google.com/123"
-    result = Urlz.Shortener.shorten(url)
+    result = Urlz.Shortener.shorten(db, url)
 
     assert result
     assert result != url
     assert String.length(result) == 4
+  end
+
+  test "expand a shortened url" do
+    {:ok, db }= Urlz.Shortener.start_link
+    url =  "http://google.com/123"
+    shortened_url = Urlz.Shortener.shorten(db, url)
+
+    result = Urlz.Shortener.expand(db, shortened_url)
+    assert result == url
   end
 end
